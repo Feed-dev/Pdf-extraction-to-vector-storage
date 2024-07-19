fdbb315d7a9b191438562929f9dc482953f3e9c0
federico Lubbert <federico.lubbert@gmail.com>
2024-07-19 15:36:03 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -127,9 +127,9 @@ class PDFVectorizer:
 
 def main():
     vectorizer = PDFVectorizer()
-    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Kabbalah'
+    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Kabbalah - Sephiroth'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "kabbalah"
+    namespace = "kabbalah sephiroth"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
