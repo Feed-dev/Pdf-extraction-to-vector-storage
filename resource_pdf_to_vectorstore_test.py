a92980bd6e08c36f61bda58e5095e29ed2b66919
federico Lubbert <federico.lubbert@gmail.com>
2024-07-19 09:33:13 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -127,9 +127,9 @@ class PDFVectorizer:
 
 def main():
     vectorizer = PDFVectorizer()
-    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Freemasonry'
+    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Gnostics'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "freemasonry"
+    namespace = "gnostics"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
