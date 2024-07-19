77a9c5b7f99c8516e4554b3a3d7d489179f9e316
federico Lubbert <federico.lubbert@gmail.com>
2024-07-19 18:54:08 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -129,7 +129,7 @@ def main():
     vectorizer = PDFVectorizer()
     pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\McLean - Ripley Scroll - Study Course'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "Ripley Scroll"
+    namespace = "ripley scroll"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
