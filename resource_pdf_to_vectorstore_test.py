40fcdd52cc4aae8d00cee333f6896f7360413af8
federico Lubbert <federico.lubbert@gmail.com>
2024-07-19 15:55:58 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -127,9 +127,9 @@ class PDFVectorizer:
 
 def main():
     vectorizer = PDFVectorizer()
-    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Kabbalah - Work of the Chariot'
+    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Kabbalah - Zohar in English'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "kabbalah work of the chariot"
+    namespace = "kabbalah zohar"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
