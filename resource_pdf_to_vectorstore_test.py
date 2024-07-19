0348e93f5bfe0c299e58d33808f4dfa7189d253b
federico Lubbert <federico.lubbert@gmail.com>
2024-07-19 13:22:59 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -127,9 +127,9 @@ class PDFVectorizer:
 
 def main():
     vectorizer = PDFVectorizer()
-    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Journal of the Society for Psychical Research'
+    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Kabbalah'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "psychical research journals"
+    namespace = "kabbalah"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
