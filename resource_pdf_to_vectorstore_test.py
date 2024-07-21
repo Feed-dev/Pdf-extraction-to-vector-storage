8c0c1b3972a294fac3a5604e4659d5197fb82e44
federico Lubbert <federico.lubbert@gmail.com>
2024-07-21 07:08:44 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -127,9 +127,9 @@ class PDFVectorizer:
 
 def main():
     vectorizer = PDFVectorizer()
-    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Order of the Golden Dawn - Hermetic'
+    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Order-of-the-Cubic-Stone'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "order of the golden dawn"
+    namespace = "order of the cubic stone"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
