115c1422482a1db0055c688c4c2e814c394f709d
federico Lubbert <federico.lubbert@gmail.com>
2024-07-21 11:05:48 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -129,7 +129,7 @@ def main():
     vectorizer = PDFVectorizer()
     pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Psychedelics'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "Psychedelics"
+    namespace = "psychedelics"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
