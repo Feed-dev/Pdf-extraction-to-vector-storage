d55109aa7c18a2d5ed38e92a5f7cdb6533247113
federico Lubbert <federico.lubbert@gmail.com>
2024-07-21 05:49:22 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -127,9 +127,9 @@ class PDFVectorizer:
 
 def main():
     vectorizer = PDFVectorizer()
-    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Order of Nine Angles - LHP'
+    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Order of the Golden Dawn - Hermetic'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "order of nine angles"
+    namespace = "order of the golden dawn"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
