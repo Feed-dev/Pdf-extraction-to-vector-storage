241f9e4979c6cda534ef6b800d225a027dd13278
federico Lubbert <federico.lubbert@gmail.com>
2024-07-22 11:38:56 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -127,9 +127,9 @@ class PDFVectorizer:
 
 def main():
     vectorizer = PDFVectorizer()
-    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Witchcraft'
+    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Zalewski - Order of the Golden Dawn'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "witchcraft"
+    namespace = "zalewski"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
