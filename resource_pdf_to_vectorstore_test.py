687d84c85b8bffe6f95c02a4230ec9c7fff88413
federico Lubbert <federico.lubbert@gmail.com>
2024-07-22 10:20:14 +0200

    added next batch

--- resource_pdf_to_vectorstore_test.py
+++ resource_pdf_to_vectorstore_test.py
@@ -127,9 +127,9 @@ class PDFVectorizer:
 
 def main():
     vectorizer = PDFVectorizer()
-    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Temple of Set - LHP - Egyptian'
+    pdf_directory = r'F:\e-boeken\the-mystic-library\Mystic_Library_A_Z\Voudon'
     index_name = vectorizer.PINECONE_INDEX_NAME
-    namespace = "temple of set"
+    namespace = "voudon"
     vectorizer.process_directory(pdf_directory, index_name, namespace)
 
 if __name__ == "__main__":
